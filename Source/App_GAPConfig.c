93c6ea5a5c841b01ad8939b61af1417fafe86d9d
chenmtech <1276094137@qq.com>
2018-09-12 12:38:58 +0800

    解决连接建立时间过长的问题；

--- Source/App_GAPConfig.c
+++ Source/App_GAPConfig.c
@@ -110,7 +110,7 @@ extern void GAPConfig_SetConnParam(uint16 minInt, uint16 maxInt, uint16 latency,
   GAPRole_SetParameter( GAPROLE_SLAVE_LATENCY, sizeof( uint16 ), &latency );
   GAPRole_SetParameter( GAPROLE_TIMEOUT_MULTIPLIER, sizeof( uint16 ), &timeout );  
   
-  GAP_SetParamValue( TGAP_CONN_EST_SUPERV_TIMEOUT, 1000);
+  GAP_SetParamValue( TGAP_CONN_EST_SUPERV_TIMEOUT, 300);
 
   //这个参数是指从连接建立后到从机更新连接参数之间需要延时的时间
   //如果主机不同意更新参数，从机可以选择断开连接或继续忍受现有参数
