294977e2156af53566807d3920eba336da3c6fae
chenmtech <1276094137@qq.com>
2020-01-07 18:03:25 +0800

    commit;

--- Source/Service_TempHumid.h
+++ Source/Service_TempHumid.h
@@ -1,50 +1,57 @@
 /**
-* 环境温湿度服务：提供实时温湿度测量，并保存过去24小时历史测量数据
+* 自定义环境温湿度服务：提供实时温湿度测量数据
 */
 
 #ifndef SERVICE_TEMPHUMID_H
 #define SERVICE_TEMPHUMID_H
 
 
-// 特征标记位
-#define TEMPHUMID_DATA          0       //实时温湿度数据
-#define TEMPHUMID_CTRL          1       //实时测量控制
-#define TEMPHUMID_PERI          2       //实时测量周期
-#define TEMPHUMID_HISTORYTIME   3       //历史测量数据的时间点
-#define TEMPHUMID_HISTORYDATA   4       //历史测量数据
+// 温湿度服务参数
+#define TEMPHUMID_DATA                   0       //实时温湿度数据
+#define TEMPHUMID_DATA_CHAR_CFG          1       //实时测量控制
+#define TEMPHUMID_INTERVAL               2       //实时测量间隔
+#define TEMPHUMID_IRANGE                 3       //实时测量间隔范围
 
 
-// 服务和特征的16位UUID
+// 服务16位UUID
 #define TEMPHUMID_SERV_UUID               0xAA60     // 温湿度服务UUID
 #define TEMPHUMID_DATA_UUID               0xAA61     // 实时温湿度数据UUID
-#define TEMPHUMID_CTRL_UUID               0xAA62     // 实时测量控制UUID
-#define TEMPHUMID_PERI_UUID               0xAA63     // 实时测量周期UUID
+#define TEMPHUMID_INTERVAL_UUID           0x2A21     // 实时测量间隔UUID
+#define TEMPHUMID_IRANGE_UUID             0x2906     // 实时测量间隔范围UUID
 
-/*历史数据的测量周期依赖计时服务中的定时周期，由定时周期控制*/
-#define TEMPHUMID_HISTORY_TIME_UUID       0xAA64      // 历史测量数据的时间UUID
-#define TEMPHUMID_HISTORY_DATA_UUID       0xAA65      // 历史测量数据UUID
 
 
 // 服务的bit field
 #define TEMPHUMID_SERVICE               0x00000001
 
 
-// 温湿度值的字节长度
-#define TEMPHUMID_DATA_LEN             8       // 两个float的长度
-
-#define TEMPHUMID_PERIOD_MIN_V         5       //最小采样周期为500ms
-
-#define TEMPHUMID_PERIOD_UNIT          100     //采样周期的单位：100ms
+// 温湿度数据的字节长度
+#define TEMPHUMID_DATA_LEN             4       
 
 
-// 需要应用层提供的回调函数声明
-// 当特征发生变化时，通知应用层
-typedef NULL_OK void (*tempHumidServiceCB_t)( uint8 paramID );
+// 回调事件
+#define TEMPHUMID_DATA_IND_ENABLED          1
+#define TEMPHUMID_DATA_IND_DISABLED         2
+#define TEMPHUMID_INTERVAL_SET              3
 
-// 需要应用层提供的回调结构体声明
+/**
+ * Thermometer Interval Range 
+ */
 typedef struct
 {
-  tempHumidServiceCB_t        pfnTempHumidServiceCB;
+  uint16 low;         
+  uint16 high; 
+} temphumidIRange_t;
+
+
+// 当某个事件发生时调用的回调函数声明
+typedef void (*tempHumidServiceCB_t)( uint8 event );
+
+
+// 由应用层提供的回调结构体声明
+typedef struct
+{
+  tempHumidServiceCB_t pfnTempHumidServiceCB;
 } tempHumidServiceCBs_t;
 
 
